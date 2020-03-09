7faa5d8b880388a9a93236cd680106b27c839762
memeboixd <43546762+memeboixd@users.noreply.github.com>
2020-03-09 01:22:58 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,83 @@
+
+
+function sroll_function() {
+
+scrollTo(0,870)
+
+
+}
+function f() {
+
+
+
+}
+function ciekawostka() {
+    scrollTo(0,1100)
+}
+function wykonawca() {
+    scrollTo(0,1230)
+
+}
+function tekst() {
+    scrollTo(0,1640)
+
+}
+function ciekawostka2() {
+    scrollTo(0,1100)
+}
+function wykonawca2() {
+    scrollTo(0,1220)
+
+}
+function tekst2() {
+    scrollTo(0,1320)
+
+}
+function ciekawostka3() {
+    scrollTo(0,1090)
+}
+function wykonawca3() {
+    scrollTo(0,1220)
+
+}
+function tekst3() {
+    scrollTo(0,1320)
+
+}
+function ciekawostka4() {
+    scrollTo(0,1110)
+}
+function wykonawca4() {
+    scrollTo(0,1220)
+
+}
+function tekst4() {
+    scrollTo(0,1560)
+
+}
+function ciekawostka5() {
+    scrollTo(0,1100)
+}
+function wykonawca5() {
+    scrollTo(0,1210)
+
+}
+function tekst5() {
+    scrollTo(0,1680)
+
+}
+function ciekawostka6() {
+    scrollTo(0,1100)
+}
+function wykonawca6() {
+    scrollTo(0,1210)
+
+}
+function tekst6() {
+    scrollTo(0,1370)
+
+}
+
+
+
+
